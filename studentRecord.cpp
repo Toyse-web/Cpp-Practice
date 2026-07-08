e906048694ef51c1375ca06e8a22f8baad3a216e
Toyse-web <olayonwatoyib05@gmail.com>
2026-07-08 18:32:51 +0100

    Refactor StudentRecord class and add display method
    Updated the StudentRecord class to improve variable naming and added a display method for output.

--- studentRecord.cpp
+++ studentRecord.cpp
@@ -4,45 +4,48 @@
 using namespace std;
 
 class StudentRecord {
-	private: 
-	string stdName;
-	int stdAge;
-	double stdGpa;
+	private:
+		string name;
+		int age;
+		double gpa;
 	
-	public: StudentRecord(string name, int age, double gpa){
-		stdName = name;
-		setAge(age);
-		setGpa(gpa);
-	}
-	
-	void setAge(int age) {
-		if (age >= 0 && age <= 120) stdAge = age;
-		else stdAge = 0;
-	}
-	void setGpa(double gpa) {
-		if (gpa >= 0 && gpa <= 4) stdGpa = gpa;
-		else stdGpa = 0;
-	}
-	
-	string getName() const {
-		return stdName;
-	}
-	
-	int getAge() const {
-		return stdAge;
-	}
-	
-	double getGpa() const {
-		return stdGpa;
-	}
+	public:
+		StudentRecord(string stdName, int stdAge, double stdGpa) : name(stdName)
+		{
+			setAge(stdAge);
+			setGpa(stdGpa);
+		}
+		
+		void setAge(int newAge) {
+			if (newAge > 0 && newAge < 120) age = newAge;
+			else age = 0;
+		}
+		
+		void setGpa(double newGpa) {
+			if (newGpa >= 0 && newGpa <= 4) gpa = newGpa;
+			else gpa = 0;
+			}
+		
+	string getName() const { return name; }
+		
+		int getAge() const { return age; }
+		
+		double getGpa() const { return gpa; }
+		
+		void display() const {
+			cout << "Name: " << name << endl;
+			cout << "Age: " << age << endl;
+			cout << "Gpa: " << gpa << endl;
+			cout << endl;
+		}
 };
 
 int main() {
-	StudentRecord student1("Toyse", 100, 3.95);
+	StudentRecord student1("Toyse", 230, 5.2);
+	StudentRecord student2("Ama", 20, 4.8);
 	
-	cout << "Name: " << student1.getName() << endl;
-	cout << "Age: " << student1.getAge() << endl;
-	cout << "Gpa: " << student1.getGpa();
+	student1.display();
+	student2.display();
 	
 	return 0;
 }
